136dad70645095e4ebac26cd523fec19a88cbae6
Santiago González <santiago.publicmail@gmail.com>
2020-04-24 03:43:55 +0000

    Make Changes

--- Makefile
+++ Makefile
@@ -2,9 +2,21 @@ HUGO_BIN=hugo
 
 .PHONY: prepare release build demo clean
 
-BASE = "/"
 staging: prepare
-	$(HUGO_BIN) server --buildDrafts --source demo --baseURL $(BASE) --appendPort=false
+	make staging_server
+	make staging_prepare
+
+staging_server:
+	$(HUGO_BIN) server --buildDrafts --source demo --baseURL / --appendPort=false --watch --renderToDisk true &
+
+staging_prepare:
+ifeq ($(RSYNC_PERSIST),1)
+	mkdir -p demo/themes/hugo-coder
+	rsync -av exampleSite/ demo
+	rsync -av --exclude='demo' --exclude='exampleSite' --exclude='.git' . demo/themes/hugo-coder
+	sleep 5s
+	make staging_prepare
+endif
 
 build: prepare
 	$(HUGO_BIN) --source demo
---
+++ exampleSite/static/js/custom.js
@@ -0,0 +1,52 @@
+const body = document.body;
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+if (localStorage.getItem("colorscheme")) {
+  if (localStorage.getItem("colorscheme") == "dark"){
+    setDarkTheme();
+  } else {
+    setLightTheme();
+  }
+} else if (darkModeToggle.matches) {
+  setDarkTheme();
+} else {
+  setLightTheme();
+}
+
+darkModeToggle.addEventListener('click', function(event) {
+  if (body.classList.contains("colorscheme-auto")) {
+    body.classList.remove('colorscheme-auto');
+    if(darkModeMediaQuery.matches) {
+        setLightTheme();
+    } else {
+        setDarkTheme();
+    }
+  } else {
+    if (body.classList.contains("colorscheme-dark")) {
+        setLightTheme();
+    } else {
+        setDarkTheme();
+    }
+  }
+});
+
+//darkModeMediaQuery.addListener((e) => {
+//  if (e.matches) {
+//    setDarkTheme();
+//  } else {
+//    setLightTheme();
+//  }
+//});
+
+function setDarkTheme() {
+  localStorage.setItem("colorscheme", "dark");
+  body.classList.add('colorscheme-dark');
+  body.classList.remove('colorscheme-light');
+}
+
+function setLightTheme() {
+  localStorage.setItem("colorscheme", "light");
+  body.classList.add('colorscheme-light');
+  body.classList.remove('colorscheme-dark');
+}
