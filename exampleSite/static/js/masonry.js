98fb5889254a0e729a50e5809335e2a07f8e8d7b
Santiago González <santiago.publicmail@gmail.com>
2021-01-25 22:52:48 +0000

    New masonry concept

--- exampleSite/static/js/masonry.js
+++ exampleSite/static/js/masonry.js
@@ -12,11 +12,11 @@
  */
 function resizeMasonryItem(item){
   /* Get the grid object, its row-gap, and the size of its implicit rows */
-  var grid = document.getElementsByClassName('masonry')[0];
+  var grid = document.getElementsByClassName('gallery-container')[0];
   if( grid ) {
     var rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap')),
         rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows')),
-        gridImagesAsContent = item.querySelector('img.masonry-content');
+        gridImagesAsContent = item.querySelector('img.gallery-item');
 
     /*
      * Spanning for any brick = S
@@ -27,12 +27,12 @@ function resizeMasonryItem(item){
      * Net height of the implicit row-track = T = G + R
      * S = H1 / T
      */
-    var rowSpan = Math.ceil((item.querySelector('.masonry-content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+    var rowSpan = Math.ceil((item.getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
 
     /* Set the spanning as calculated above (S) */
     item.style.gridRowEnd = 'span '+rowSpan;
     if(gridImagesAsContent) {
-      item.querySelector('img.masonry-content').style.height = item.getBoundingClientRect().height + "px";
+      item.querySelector('img.gallery-item').style.height = item.getBoundingClientRect().height + "px";
     }
   }
 }
@@ -48,7 +48,7 @@ function resizeMasonryItem(item){
  */
 function resizeAllMasonryItems(){
   // Get all item class objects in one list
-  var allItems = document.querySelectorAll('.masonry-item');
+  var allItems = document.querySelectorAll('.gallery-item');
 
   /*
    * Loop through the above list and execute the spanning function to
@@ -72,7 +72,7 @@ function resizeAllMasonryItems(){
  */
 function waitForImages() {
   //var grid = document.getElementById("masonry");
-  var allItems = document.querySelectorAll('.masonry-item');
+  var allItems = document.querySelectorAll('.gallery-item');
   if( allItems ) {
     for(var i=0;i<allItems.length;i++){
       imagesLoaded( allItems[i], function(instance) {
